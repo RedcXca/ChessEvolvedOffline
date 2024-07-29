8836f602c8bd177e12edc7afc9bf5c671f300870
Max Lu <maxzylu@gmail.com>
2024-07-29 02:11:40 -0400

    updated textobserver to display board properly

--- src/TextObserver.cc
+++ src/TextObserver.cc
@@ -6,11 +6,11 @@ void TextObserver::update() {
     if (!game) return;
 
     for (int i = Board::SIZE - 1; i >= 0; i--) {
-        std::cout << i << "  ";
+        std::cout << i + 1 << "  ";
         for (int j = 0; j < Board::SIZE; j++) {
             std::cout << game->getState(Position(j, i)) << ' ';
         }
         std::cout << std::endl;
     }
-    std::cout << "\n  a b c d e f g h" << std::endl;
+    std::cout << "\n   a b c d e f g h" << std::endl;
 }
