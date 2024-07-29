c12f4f70af9869c463b4dac8af0604e31a67d51c
w4zEl <proximitytrigger@gmail.com>
2024-07-28 20:47:44 -0400

    Improve Subject::detach

--- src/Subject.cc
+++ src/Subject.cc
@@ -1,16 +1,12 @@
 #include "Subject.h"
 #include "Observer.h"
+#include <algorithm>
 
 void Subject::attach(Observer* o) {
     observers.emplace_back(o);
 }
 void Subject::detach(Observer* o) {
-    for (auto it = observers.begin(); it != observers.end(); it++) {
-        if (*it == o) {
-            observers.erase(it);
-            break;
-        }
-    }
+    observers.erase(std::find(observers.begin(), observers.end(), o));
 }
 void Subject::notifyObservers() {
     for (auto& o : observers) o->update();
