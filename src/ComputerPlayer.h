63d9fa1fdeb8dcbb992cf8a6852c2087eddd2631
w4zEl <proximitytrigger@gmail.com>
2024-07-29 13:51:54 -0400

    Rename Computer to ComputerPlayer, fix randomMove method.

---
+++ src/ComputerPlayer.h
@@ -0,0 +1,15 @@
+#include "color.h"
+#include "Player.h"
+#include <random>
+
+struct ComputerPlayer : Player {
+    ComputerPlayer(Color color) : color{color} {}
+
+protected:
+    Color color;
+    MoveInput randomMove(const std::vector<Move>& moves) {
+        static std::mt19937 gen(std::random_device{}());
+        auto randMove = moves[std::uniform_int_distribution<>(0, moves.size() - 1)(gen)];
+        return {randMove.from, randMove.to, randMove.promotionPiece};
+    }
+};
