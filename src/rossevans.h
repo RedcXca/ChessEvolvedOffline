5b1757910ffff2eca1613ac80322ab44449a41dc
Max Lu <maxzylu@gmail.com>
2024-07-28 22:37:32 -0400

    fixed missing info in some pieces

--- src/rossevans.h
+++ src/rossevans.h
@@ -9,5 +9,6 @@ public:
     char toChar() override;
     int getValue() override;
     std::vector<PartialMove> getPossibleMoves() override;
+    using Piece::Piece;
 };
-#endif;
+#endif
--- src/wyvern.cc
+++ src/wyvern.cc
@@ -11,6 +11,6 @@ std::vector<PartialMove> Wyvern::getPossibleMoves() {
     std::vector<PartialMove> moves;
     pushRookMoves(moves, 1, MoveType::MoveOrAttack);
     pushRookMoves(moves, 2, MoveType::AttackOnly, 2);
-    pushBishopMoves(moves, 2, MoveType::Teleport), 2;
+    pushBishopMoves(moves, 2, MoveType::Teleport, 2);
     return moves;
 }
