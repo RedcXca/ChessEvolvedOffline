25051d9e4baa0b3459e61a898bf74b0ec0d4276f
Jason Cheng <jcheng3927@gmail.com>
2024-07-29 17:59:16 -0400

    level 3

--- src/ComputerLevel3.cc
+++ src/ComputerLevel3.cc
@@ -3,14 +3,22 @@
 // TODO: UNIMPLEMENTED
 MoveInput ComputerLevel3::getNextMove(Board& board) {
     auto& moves = board.getLegalMoves();
-    std::vector<Move> priorityMoves;
-    priorityMoves.reserve(moves.size());
+    std::vector<Move> avoidCaptures;
+    avoidCaptures.reserve(moves.size());
+    std::vector<Move> captureChecks;
+    captureChecks.reserve(moves.size());
     for (const Move& move : moves) {
-        if (move.capturedPiece) {
-            priorityMoves.push_back(move);
+        int priorThreats = board.checkThreatened(move.from).at(board.getNextColor(board.getSide()));
+        board.makeMove(move);
+        int postThreats = board.checkThreatened(move.to).at(board.getSide());
+        board.undoMove();
+        if (postThreats < priorThreats) {
+            avoidCaptures.push_back(move);
+        } else if (move.capturedPiece) {
+            captureChecks.push_back(move);
         } else if (move.check) {
-            priorityMoves.push_back(move);
+            captureChecks.push_back(move);
         }
     }
-    return randomMove(priorityMoves.size() ? priorityMoves : moves);
+    return randomMove(avoidCaptures.size() ? avoidCaptures : captureChecks.size() ? captureChecks : moves);
 }
