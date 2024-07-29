2b66bd658ea8456a22f60384f9ef73464270ab5b
w4zEl <proximitytrigger@gmail.com>
2024-07-28 20:47:29 -0400

    Start main.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Game.h"
 #include "Observer.h"
 #include "TextObserver.h"
@@ -9,7 +10,21 @@ int main() {
     Game game;
     TextObserver textObserver(&game);
     GraphicsObserver graphicsObserver(&game);
+    bool activeGame = false;
 
+    for (std::string command; std::cin >> command;) {
+        if (command == "game") {
+            if (activeGame) std::cerr << "There is already an active game.\n";
+            else {
+                activeGame = true;
+            }
+        } else if (command == "setup") {
+            if (activeGame) std::cerr << "There is already an active game.\n";
+            else {
+                game.setup();
+            }
+        } else std::cerr << "Invalid command!\n";
+    }
     // game.attach(textObserver);
     // TODO: attach graphicsObserver
     // TODO: game.play();
