6703715ab22802822801ab1c56d59e481fbe500b
w4zEl <proximitytrigger@gmail.com>
2024-07-30 21:41:25 -0400

    Minor cleaning.

--- src/position.cc
+++ src/position.cc
@@ -3,7 +3,7 @@
 
 Position::Position(int x, int y) : x(x), y(y) {}
 
-Position::Position(const std::string& str) : x(str.size() != 2 ? throw ChessException{"Position must be exactly 2 characters."} : 'a' <= str[0] && str[0] <= 'h' ? str[0] - 'a' : throw ChessException{"Invalid file."} ), y('1' <= str[1] && str[1] <= '8' ? str[1] - '1' : throw ChessException{"Invalid rank."}) {}
+Position::Position(const std::string& str) : x(str.size() != 2 ? throw ChessException{"Position must be exactly 2 characters."} : 'a' <= str[0] && str[0] <= 'h' ? str[0] - 'a' : throw ChessException{"Invalid file."}), y('1' <= str[1] && str[1] <= '8' ? str[1] - '1' : throw ChessException{"Invalid rank."}) {}
 
 std::string Position::toString() {
     return char(x + 'a') + std::to_string(y + 1);
