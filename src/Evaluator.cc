1204a21aafb7a91c9d07962f39d49a56e55b8657
w4zEl <proximitytrigger@gmail.com>
2024-07-30 20:17:47 -0400

    Simplify Evaluator, use UnrecoverableChessException.

--- src/Evaluator.cc
+++ src/Evaluator.cc
@@ -1,16 +1,14 @@
 #include "Evaluator.h"
+#include "UnrecoverableChessException.h"
+
 Evaluator::Evaluator(char pieceType): pieceType{pieceType}{
-    std::ifstream file("./evaluations/" + std::string(1, pieceType) + ".txt");
-    if (!file.is_open()) {
-        throw std::runtime_error("Could not open file evaluations/" + std::string(1, pieceType) + ".txt");
-    }
-    for (int i = 0; i < Board::SIZE; ++i) {
-        for (int j = 0; j < Board::SIZE; ++j) {
-            if (!(file >> evals[i][j])) {
-                throw std::runtime_error("Error reading file");
-            }
-        }
-    }
+    std::ifstream file(std::string{"./evaluations/"} + pieceType + ".txt");
+    if (!file.is_open())
+        throw UnrecoverableChessException{"Could not open file evaluations/" + std::string(1, pieceType) + ".txt"};
+    for (int i = 0; i < Board::SIZE; ++i)
+        for (int j = 0; j < Board::SIZE; ++j)
+            if (!(file >> evals[i][j]))
+                throw UnrecoverableChessException{"Error reading file"};
 }
 
 int Evaluator::getEval(Position p, Color c){
